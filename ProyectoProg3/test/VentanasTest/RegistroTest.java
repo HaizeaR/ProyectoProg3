2c6d003af9ec69701a9774bc48862a596123236d
Haizea <Haizea@192.168.0.6>
2019-11-16 18:58:23 +0100

    Solucion problemas de estructura

---
+++ ProyectoProg3/test/VentanasTest/RegistroTest.java
@@ -0,0 +1,25 @@
+package VentanasTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Ventanas.Registro;
+
+public class RegistroTest {
+
+	@Test
+	public void compruebaCorreo() {
+		assertTrue(Registro.comprobarCorreo("universidad@deusto.com", false));
+		assertTrue(Registro.comprobarCorreo("usuario123@ejemplo.es", false));
+		assertFalse(Registro.comprobarCorreo("universidad.com", false));
+		assertFalse(Registro.comprobarCorreo("ejemplo@error", false));
+	}
+	
+	@Test
+	public void compruebaNumTarjeta() {
+		assertTrue(Registro.comprobarNumTarjeta("123456", false));
+		assertFalse(Registro.comprobarNumTarjeta("12aB4h", false));
+	}
+
+}
