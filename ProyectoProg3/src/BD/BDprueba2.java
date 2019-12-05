ad13ce9531bb26f42b5becbef68b6e04fb4ed576
Haizea <Haizea@10.105.145.197>
2019-12-05 15:26:36 +0100

    BD

--- ProyectoProg3/src/BD/BDprueba2.java
+++ ProyectoProg3/src/BD/BDprueba2.java
@@ -30,25 +30,25 @@ public class BDprueba2 {
 			
 			
 			Statement statement = conexion.createStatement();
-			String sent = "CREATE TABLE IF NOT EXISTS cliente (dni INTEGER PRIMARY KEY, nombre varchar(100), apellido varchar(100), correo varchar(100), contrasena varchar(20), n_tarjeta int(16));";
+			String sent = "CREATE TABLE IF NOT EXISTS cliente (dni varchar(9) PRIMARY KEY, nombre varchar(100), apellido varchar(100), correo varchar(100), contrasena varchar(20), n_tarjeta int(16));";
 			System.out.println( sent );
 			statement.executeUpdate( sent );
 			
 			// REVISAR
 
-			sent = "CREATE TABLE IF NOT EXISTS admin (dni INTEGER PRIMARY KEY, nombre varchar(100), apellido varchar(100), correo varchar(100), contrasena varchar(20);";
+			sent = "CREATE TABLE IF NOT EXISTS admin (dni varchar(9) PRIMARY KEY, nombre varchar(100), apellido varchar(100), correo varchar(100), contrasena varchar(20));";
 			System.out.println( sent );
 			statement.executeUpdate( sent );
 			
-			sent = "CREATE TABLE IF NOT EXISTS asiento (codigo INTEGER PRIMARY KEY, fila int(2), columna int(2), ocupado boolean;";
+			sent = "CREATE TABLE IF NOT EXISTS asiento (codigo INTEGER PRIMARY KEY, fila int(2), columna int(2), ocupado boolean);";
 			System.out.println( sent );
 			statement.executeUpdate( sent );
 			
-			sent = "CREATE TABLE IF NOT EXISTS pelicula (cod_peli INTEGER PRIMARY KEY, titulo_peli varchar(100), descrip_peli varchar(100), duracion_peli int (3);";
+			sent = "CREATE TABLE IF NOT EXISTS pelicula (cod_peli INTEGER PRIMARY KEY, titulo_peli varchar(100), descrip_peli varchar(100), duracion_peli int (3));";
 			System.out.println( sent );
 			statement.executeUpdate( sent );
 			
-			sent = "CREATE TABLE IF NOT EXISTS sala (numero_sala INTEGER PRIMARY KEY, capacidad sala int(3), ID_cine int(3);";
+			sent = "CREATE TABLE IF NOT EXISTS sala (numero_sala INTEGER PRIMARY KEY, capacidad sala int(3), ID_cine int(3));";
 			System.out.println( sent );
 			statement.executeUpdate( sent );
 			
